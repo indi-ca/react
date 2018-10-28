cdb577d9802c3438d708ae2ddb996a3d2faff1dd
Indika Piyasena <indika.piyasena@gmail.com>
2018-10-28 22:50:06 +0300

    react tutorial

--- src/App.js
+++ src/App.js
@@ -2,27 +2,68 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class Fragment extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and ahorrar to reload...
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Fragment">
+        <div>Title {this.props.value}</div>
+        <div>Description</div>
+        <div>Empty space</div>
+        <div>Examples</div>
+        <button className="square" onClick={() => this.props.onClick()}>
+          {this.props.value}
+        </button>
       </div>
     );
   }
 }
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+      fragments: Array(3).fill(1),
+    };
+  }
+
+  handleClick(i) {
+    const fragments = this.state.fragments.slice();
+    fragments[i] = '3';
+    this.setState({fragments: fragments});
+  }
+
+  renderFragment(i) {
+    return (
+      <Fragment
+        value={this.state.fragments[i]}
+        onClick={() => this.handleClick(i)}
+      />);
+  }
+
+  render() {
+    return (
+      <div className="Container">
+        <button className="refresh" onClick={() => this.setState({ value: 'Refreshing' })}>
+          {this.state.value}
+        </button>
+        {this.renderFragment(0)}
+        {this.renderFragment(1)}
+        {this.renderFragment(2)}
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Container />
+    );
+  }
+}
+
+
 
 export default App;
